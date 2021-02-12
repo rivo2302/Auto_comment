fcba261db856b2fb6d6762a00601b9ed34323bbb
rivo2302 <rivo2302@gmail.com>
2021-02-12 15:01:20 +0300

    Remove some files

--- config.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flaskext.mysql import MySQL
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'hackathon'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
